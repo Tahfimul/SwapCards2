1656e19af936cb948693059726e8aec96bdc39e7
Tahfimul <victory.josh.josh24@gmail.com>
2018-10-30 13:11:05 -0400

    Update

--- app/src/main/java/com/example/nycgtrcode/codepathlab2/AddCardActivity.java
+++ app/src/main/java/com/example/nycgtrcode/codepathlab2/AddCardActivity.java
@@ -3,16 +3,17 @@ package com.example.nycgtrcode.codepathlab2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 public class AddCardActivity extends AppCompatActivity {
 
     ImageView closeBtn, saveBtn;
-    EditText question;
-    EditText answer;
+    EditText question, aCorrect,  a, a1, a2, a3;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +21,27 @@ public class AddCardActivity extends AppCompatActivity {
         closeBtn = findViewById(R.id.btn_close);
         saveBtn = findViewById(R.id.save_btn);
         question = findViewById(R.id.question_prompt);
-        answer = findViewById(R.id.answer_prompt);
+        aCorrect = findViewById(R.id.answer_prompt_correct);
+        a = findViewById(R.id.answer_prompt);
+        a1 = findViewById(R.id.answer_prompt1);
+        a2 = findViewById(R.id.answer_prompt2);
+        a3 = findViewById(R.id.answer_prompt3);
+        checkIntentData();
         saveBtnClick();
         closeBtnClick();
+    }
+
+    private void checkIntentData() {
+        Intent i = getIntent();
+        if (i.getStringExtra("q")!=null&&i.getStringExtra("aCorrect")!=null&&i.getStringExtra("a")!=null&&i.getStringExtra("a1")!=null&&i.getStringExtra("a2")!=null&&i.getStringExtra("a3")!=null)
+        {
+            question.setText(i.getStringExtra("q"));
+            aCorrect.setText(i.getStringExtra("aCorrect"));
+            a.setText(i.getStringExtra("a"));
+            a1.setText(i.getStringExtra("a1"));
+            a2.setText(i.getStringExtra("a2"));
+            a3.setText(i.getStringExtra("a3"));
+        }
     }
 
     private void saveBtnClick() {
@@ -30,32 +49,54 @@ public class AddCardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String q = question.getText().toString();
-                String a = answer.getText().toString();
-                if (verifyInputs(q, a))
-                    passUserInput(q , a);
+                String aC = aCorrect.getText().toString();
+                String A = a.getText().toString();
+                String A1 = a1.getText().toString();
+                String A2 = a2.getText().toString();
+                String A3 = a3.getText().toString();
+                if (verifyInputs(q, aC, A ,A1, A2, A3))
+                    passUserInput(q , aC, A, A1, A2, A3);
             }
         });
     }
 
-    private boolean verifyInputs(String q, String a) {
+    private boolean verifyInputs(String q, String aC, String A, String A1, String A2, String A3) {
         if (q.length()<1)
             question.setError("Field can't be left empty");
 
-        else if(a.length()<1)
-            answer.setError("Field can't be left empty");
+        else if(aC.length()<1)
+            aCorrect.setError("Field can't be left empty");
 
-        if (q.length()>=1&&a.length()>=1)
+        else if(A.length()<1)
+            a.setError("Field can't be left empty");
+        else if(A1.length()<1)
+            a1.setError("Field can't be left empty");
+        else if(A2.length()<1)
+            a2.setError("Field can't be left empty");
+        else if(A3.length()<1)
+            a3.setError("Field can't be left empty");
+        else if(!A.equals(aC)||!A1.equals(aC)||!A2.equals(aC)||!A3.equals(aC))
+            Snackbar.make(findViewById(R.id.addCardView), "Please enter a correct answer for the choices", Snackbar.LENGTH_LONG).show();
+
+
+        if (q.length()>=1&&aC.length()>=1&&A.length()>=1&&A1.length()>=1&&A2.length()>=1&&A3.length()>=1&&(A.equals(aC)||A1.equals(aC)||A2.equals(aC)||A3.equals(aC)))
             return true;
 
         return false;
 
     }
 
-    private void passUserInput(String q, String a) {
+    private void passUserInput(String q, String aC, String A, String A1, String A2, String A3) {
+
+        System.out.println(aC + "\t:aC Data");
 
         Intent mainActivity = new Intent(AddCardActivity.this, MainActivity.class);
         mainActivity.putExtra("q", q);
-        mainActivity.putExtra("a", a);
+        mainActivity.putExtra("aCorrect", aC);
+        mainActivity.putExtra("a", A);
+        mainActivity.putExtra("a1", A1);
+        mainActivity.putExtra("a2", A2);
+        mainActivity.putExtra("a3", A3);
         startActivity(mainActivity);
     }
 
--- app/src/main/java/com/example/nycgtrcode/codepathlab2/MainActivity.java
+++ app/src/main/java/com/example/nycgtrcode/codepathlab2/MainActivity.java
@@ -1,38 +1,80 @@
 package com.example.nycgtrcode.codepathlab2;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    ImageView addBtn;
-    TextView output;
+    ImageView addBtn, editBtn;
+    TextView output, A, A1, A2, A3;
     boolean showQuestion=true, showAnswer=false;
-    String q, a;
+    String q, aCorrect ,a, a1, a2, a3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addBtn = findViewById(R.id.add_btn);
+        editBtn = findViewById(R.id.edit_btn);
         output = findViewById(R.id.output);
+        A = findViewById(R.id.a);
+        A1 = findViewById(R.id.a1);
+        A2 = findViewById(R.id.a2);
+        A3 = findViewById(R.id.a3);
+        checkIntentData();
+        editBtnClick();
+        addBtnClick();
+        output.setText(q);
+        setOutputTextViewClick();
+        setChoices();
+        setOnClickChoices();
+    }
+    private void checkIntentData() {
         Intent i = getIntent();
-        if(i.getStringExtra("q")==null||i.getStringExtra("a")==null)
+        if(i.getStringExtra("q")==null||i.getStringExtra("aCorrect")==null||i.getStringExtra("a")==null||i.getStringExtra("a1")==null||i.getStringExtra("a2")==null||i.getStringExtra("a3")==null)
         {
             q = "Who is the 44th president of the United States?";
+            aCorrect = "Barack Obama";
             a = "Barack Obama";
+            a1 = "George W. Bush";
+            a2 = "Bill Gates";
+            a3 = "None";
+
         }
         else
         {
             q= i.getStringExtra("q");
+            aCorrect = i.getStringExtra("aCorrect");
             a=i.getStringExtra("a");
+            a1 = i.getStringExtra("a1");
+            a2 = i.getStringExtra("a2");
+            a3 = i.getStringExtra("a3");
+            Snackbar.make(findViewById(R.id.activityMain), "New Card Created", Snackbar.LENGTH_LONG).show();
+
         }
-        addBtnClick();
-        output.setText(q);
-        setOutputTextViewClick();
+    }
+
+    private void editBtnClick() {
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent editCard = new Intent(MainActivity.this, AddCardActivity.class);
+                editCard.putExtra("q", q);
+                editCard.putExtra("aCorrect", aCorrect);
+                editCard.putExtra("a", a);
+                editCard.putExtra("a1", a1);
+                editCard.putExtra("a2", a2);
+                editCard.putExtra("a3", a3);
+                startActivity(editCard);
+            }
+        });
+
     }
 
     private void addBtnClick() {
@@ -64,4 +106,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void setChoices() {
+        A.setText(a);
+        A1.setText(a1);
+        A2.setText(a2);
+        A3.setText(a3);
+    }
+
+
+    private void setOnClickChoices() {
+        A.setOnClickListener(this);
+        A1.setOnClickListener(this);
+        A2.setOnClickListener(this);
+        A3.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+       Button val = (Button) v;
+       String valText = val.getText().toString();
+
+       if(!valText.equals(aCorrect))
+           val.setBackground(getResources().getDrawable(R.color.colorRed));
+
+       else
+           val.setBackground(getResources().getDrawable(R.color.colorGreen));
+
+
+
+
+    }
 }
+
